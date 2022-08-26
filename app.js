48a2270bb330d65909de7ca03d87ccc62ad68675
Rishant <Rishant@RishantPC>
2022-08-27 01:32:24 +0530

    add start script

--- app.js
+++ app.js
@@ -129,6 +129,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Listening on 3000 port');
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on ${port} port`);
 })
