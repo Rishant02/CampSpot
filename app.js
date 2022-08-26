55046b02f45508b74c4d3e963c15020a70c8a40a
Rishant <Rishant@RishantPC>
2022-08-27 01:22:11 +0530

    Added basic security

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose')
@@ -6,16 +9,20 @@ const methodOverride = require('method-override');
 const session = require('express-session')
 const flash = require('connect-flash')
 const ExpressError = require('./utils/expressError')
-
+const mongoSanitize = require('express-mongo-sanitize')
 const passport = require('passport')
 const localStrategy = require('passport-local')
 const User = require('./models/user');
+const helmet = require('helmet')
+const { scriptSrcUrls, styleSrcUrls, connectSrcUrls, fontSrcUrls } = require('./utils/srcUrls')
+const MongoStore = require('connect-mongo');
 
 const campgroundRouter = require('./routes/campground')
 const reviewRouter = require('./routes/review')
 const userRouter = require('./routes/user')
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yele-camp';
 
-mongoose.connect('mongodb://localhost:27017/yele-camp', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -32,19 +39,57 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize());
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: secret
+    }
+});
+
+store.on("error", function (e) {
+    console.log('session store error', e)
+})
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
+    store,
+    name: 'session',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure:true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dw0gla16o/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize())
 app.use(passport.session())
