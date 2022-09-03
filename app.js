31743632f92a38db3a0c367e105df60562ccb9cd
Rishant <Rishant@RishantPC>
2022-09-03 21:53:25 +0530

    added pagination

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ const MongoStore = require('connect-mongo');
 const campgroundRouter = require('./routes/campground')
 const reviewRouter = require('./routes/review')
 const userRouter = require('./routes/user')
-const dbUrl = 'mongodb://localhost:27017/yele-camp';
-// const dbUrl = process.env.DB_URL ||
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yele-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
